e5072d3bafe97122d3b2b553320e2cf17499567c
Eduardo6446 <eduorozco8@gmail.com>
2023-04-19 21:24:44 -0600

    lab5

---
+++ app/src/main/java/com/example/artelista/Activity_menu.kt
@@ -0,0 +1,35 @@
+package com.example.artelista
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.artelista.databinding.ActivityMenuBinding
+
+class Activity_menu : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_menu)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navHome, R.id.navEvento, R.id.navArte, R.id.navArtista
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
